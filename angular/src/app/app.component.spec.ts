b440d0732b1d66dcef149b424ca9e8425542855f
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2022-03-17 15:53:56 -0500

    Attempting to fix hanging karma server, update eslint

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -3,42 +3,48 @@ import {
   TestBed,
   fakeAsync,
   tick,
-  waitForAsync
+  waitForAsync, flushMicrotasks
 } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('AppComponent', (): void => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
-  beforeAll(() => {
-    window.onbeforeunload = () =>
+  beforeAll((): void => {
+    window.onbeforeunload = (): string =>
       "Set this so that you don't get" +
       '"Some of your tests did a full page reload!!" error';
   });
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(waitForAsync((): void => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
-      providers: []
+      providers: [],
+      teardown: {
+        destroyAfterEach: true
+      }
     })
       .compileComponents()
-      .then(() => {
+      .then((): void => {
         fixture = TestBed.createComponent(AppComponent);
         component = fixture.componentInstance;
       });
   }));
 
-  it(`should have as title 'angular-example'`, () => {
-    // const fixture = TestBed.createComponent(AppComponent);
+  afterEach((): void => {
+    fixture.destroy();
+    component = null;
+  });
+
+  it(`should have as title 'angular-example'`, (): void => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular-example');
   });
 
-  it('should render title in a h1 tag', () => {
-    // const fixture = TestBed.createComponent(AppComponent);
+  it('should render title in a h1 tag', (): void => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain(
@@ -46,7 +52,7 @@ describe('AppComponent', () => {
     );
   });
 
-  it('should do stuff', fakeAsync(() => {
+  it('should do stuff', fakeAsync((): void => {
     spyOn(component, 'doStuff').and.callThrough();
     const anchor = fixture.debugElement.nativeElement.querySelector('a');
     anchor.click();
@@ -54,7 +60,7 @@ describe('AppComponent', () => {
     expect(component.doStuff).toHaveBeenCalled();
   }));
 
-  it('should do other stuff', fakeAsync(() => {
+  it('should do other stuff', fakeAsync((): void => {
     spyOn(component, 'doOtherStuff').and.callThrough();
     const anchor =
       fixture.debugElement.nativeElement.querySelector('a#doOtherStuff');
