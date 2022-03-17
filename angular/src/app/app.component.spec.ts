4e5e153d53d97ee8735354b507e0194b4d270096
Joe Tingsanchali <joe.tingsanchali@sonarsource.com>
2022-03-17 10:06:04 -0500

    Add eslint, fix unit tests with updated karma/jasmine

--- angular/src/app/app.component.spec.ts
+++ angular/src/app/app.component.spec.ts
@@ -1,41 +1,54 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import {TestBed, waitForAsync, ComponentFixture, fakeAsync, tick} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeAll(() => {
+    window.onbeforeunload = () => 'Set this so that you don\'t get' +
+        '"Some of your tests did a full page reload!!" error';
+  });
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      providers: []
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+    });
   }));
 
   it(`should have as title 'angular-example'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    // const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular-example');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    // const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-example!');
   });
 
-  it('should do lots of stuff', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    // finds and clicks the first <a> tag bound to doStuff()
-    compiled.querySelector('a').click();
-    // finds and clicks the second <a> tag bound to doOtherStuff()
-    compiled.querySelector('#doOtherStuff').click();
-    fixture.detectChanges();
-  });
+  it('should do stuff', fakeAsync(() => {
+    spyOn(component, 'doStuff').and.callThrough();
+    const anchor = fixture.debugElement.nativeElement.querySelector('a');
+    anchor.click();
+    tick();
+    expect(component.doStuff).toHaveBeenCalled();
+  }));
+
+  it('should do other stuff', fakeAsync(() => {
+    spyOn(component, 'doOtherStuff').and.callThrough();
+    const anchor = fixture.debugElement.nativeElement.querySelector('a#doOtherStuff');
+    anchor.click();
+    tick();
+    expect(component.doOtherStuff).toHaveBeenCalled();
+  }));
 
 });
